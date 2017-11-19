d3d97425fd71edb10956084b2d3a7209a03a9d79
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-19 15:38:41 -0700

    create test class for Blobs

---
+++ app/src/test/java/com/watabou/pixeldungeon/actors/blobs/BlobTest.java
@@ -0,0 +1,26 @@
+package com.watabou.pixeldungeon.actors.blobs;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arjun on 19/11/17.
+ */
+public class BlobTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSimple()    {
+        fail("BlobTest not implemented!");
+    }
+
+}
