231869de769df8528631938963d251c7e975dc9e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-26 15:23:24 -0700

    testing food execute()

--- app/src/test/java/com/watabou/pixeldungeon/items/food/FoodTest.java
+++ app/src/test/java/com/watabou/pixeldungeon/items/food/FoodTest.java
@@ -1,6 +1,8 @@
 package com.watabou.pixeldungeon.items.food;
 
+import com.watabou.pixeldungeon.actors.buffs.Hunger;
 import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.actors.hero.HeroClass;
 
 import org.junit.After;
 import org.junit.Before;
@@ -42,7 +44,48 @@ public class FoodTest {
     }
 
     @Test
-    public void execute() throws Exception {
+    public void test_execute_warrior() throws Exception {
+        hero.add(new Hunger());
+
+        class FoodValues    {
+            HeroClass hClass;
+            int hp;
+            int ht;
+            int expected;
+
+            FoodValues(HeroClass heroClass, int hp, int ht, int expected)    {
+                this.hClass = heroClass;
+                this.hp = hp;
+                this.ht = ht;
+                this.expected = expected;
+            }
+        }
+
+        FoodValues [] foodValues = new FoodValues[11];
+
+        foodValues[0] = new FoodValues(HeroClass.WARRIOR, 100, 200, 105);
+        foodValues[1] = new FoodValues(HeroClass.WARRIOR, 100, 101, 101);
+        foodValues[2] = new FoodValues(HeroClass.WARRIOR, 100, 104, 104);
+        foodValues[3] = new FoodValues(HeroClass.WARRIOR, 100, 105, 105);
+        foodValues[4] = new FoodValues(HeroClass.WARRIOR, 100, 106, 105);
+        foodValues[5] = new FoodValues(HeroClass.WARRIOR, 100, 90, 100);
+
+//        foodValues[6] = new FoodValues(HeroClass.ROGUE, 100, 200, 100);
+//        foodValues[7] = new FoodValues(HeroClass.ROGUE, 100, 104, 100);
+//        foodValues[8] = new FoodValues(HeroClass.ROGUE, 100, 105, 100);
+//        foodValues[9] = new FoodValues(HeroClass.ROGUE, 100, 106, 100);
+//        foodValues[10] = new FoodValues(HeroClass.ROGUE, 100, 90, 100);
+
+
+        for (FoodValues fv:foodValues)  {
+            hero.heroClass = fv.hClass;
+            hero.HP = fv.hp;
+            hero.HT = fv.ht;
+
+            food.execute(hero, Food.AC_EAT);
+            assertEquals(fv.expected, hero.HP);
+        }
+//        fail("not implemented");
     }
 
 }
